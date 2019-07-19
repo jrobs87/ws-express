90a573e0cc40a6a07593c5738e86e6c8a6c7eb1d
John Robertson <tiktaalikdesign@gmail.com>
2019-07-19 12:08:21 -0400

    added URL parsing for websockets routes, additional message and user props

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var expressWs = require('express-ws');
 var expressWs = expressWs(express());
 var app = expressWs.app;
 const path = require('path');
+var moment = require('moment');
+moment().format();
 
 const port = 3000;
 
@@ -12,39 +14,47 @@ app.use(bodyParser.json());
 //support parsing of application/x-www-form-urlencoded post data
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-// HTML - Home
-app.get('/', function (req, res, next) {
-    console.log('index.html sent to client')
-    res.sendFile(path.join(__dirname, './index.html'));
-});
-
 // HTML - Chat (loads the chat page)
 app.get('/', function (req, res, next) {
-    console.log('index.html sent to client')
+    console.log('Index.html sent to Client')
     res.sendFile(path.join(__dirname, './index.html'));
 });
 
 //Secure WebSocket Instance
 var aWss = expressWs.getWss('/');
 
-// WebSocket Routes and Handlers
-app.ws('/', function (ws, req) {
-    console.log(`Socket Connected on ${port}`);
+const thread = [];
+
+// WebSocket Routes and Handlers (on connection)
+app.ws('/:user', function (ws, req) {
+    ws.user = req.params.user;
+    console.log(`${ws.user} has connected on Port ${port}`);
+    // console.log(aWss.clients)
+
+    ws.on('close', req => {
+        console.log('user disconnected');
+        console.log(req)
+    });
 
     // receive message, translate, and emit to all connected clients
     ws.onmessage = function (msg) {
-        console.log(msg.data);
+        // we have to parse json manually bc bodyParser() only works on req.bdoy, which is not available to ws
+        message = JSON.parse(msg.data)
+        console.log(`${message.userName} (${message.userID}): "${message.msg}"`)
 
         // ----- Google API ----- //
         // Place API call here.  
         // Use data.msg for the text 
         // Use data.target for langauge ('es' is hardcoded)
         // ---------------------- //
+        msg.id = 'id';
 
         // iterate over clients/connected sockets to broadcast message
         aWss.clients.forEach(function (client) {
             client.send(msg.data);
+            thread.push(msg.data);
+            // console.log(thread);
+            // console.log(aWss.clients)
         });
     };
 });
